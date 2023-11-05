089053a101e3383c9d4030b49089ab816fd233fd
rahat2020 <kazimdboktiar2020@gmail.com>
2023-11-05 14:21:26 +0600

    array delete and multiple items delete implementd

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,78 +1,126 @@
-import { Container, Row, Col, Image, Form } from "react-bootstrap"
+import { Container, Row, Col, Image, Form, Button } from "react-bootstrap"
 import { useState, useEffect } from "react";
 import { photoData } from "../data/photoData";
 import SortableList, { SortableItem } from 'react-easy-sort'
-import { arrayMoveImmutable } from 'array-move'
+import { arrayMoveImmutable } from 'array-move';
+
+type PhotoArrya = {
+    id: number,
+    photo: string
+}
 
 const Home = () => {
-    const [data, setPhotoData] = useState<{ id: number; photo: string; }[]>([]);
     const [items, setItems] = useState(photoData)
+
     const onSortEnd = (oldIndex: number, newIndex: number) => {
         setItems((array) => arrayMoveImmutable(array, oldIndex, newIndex))
+    }
+
+    const [selectedItems, setSelectedItems] = useState<{ id: number; photo: string; }[]>([]);
+
+
+    // console.log('checkedItems', checkedItems)
+    // console.log('selectedItems', selectedItems)
+
+    // selected images
+    const handleCheckboxChange = (item: { id: number; photo: string; }) => {
+        const isSelected = selectedItems.some((selectedItem) => selectedItem.id === item.id);
+        if (isSelected) {
+            const updatedSelectedItems = selectedItems.filter((selectedItem) => selectedItem.id !== item.id);
+            setSelectedItems(updatedSelectedItems);
+        } else {
+            setSelectedItems([...selectedItems, item]);
+        }
+    };
+
+    //   delete image
+    // const handleDelete = () => {
+    //     const newArray = items;
+    //     const selectedPhoto = selectedItems.map(item => { return item.id })
+    //     console.log('selectedItems', selectedPhoto)
+    //     const filteredArray = newArray.filter((elm, i) => {
+    //         return i !== selectedPhoto 
+
+    //     })
+    //     setItems(filteredArray)
+    // }
+    const handleDelete = () => {
+        const selectedPhotoIds = selectedItems.map(item => item.id);
+        console.log('selectedItems', selectedPhotoIds);
+        
+        const filteredArray = items.filter(item => !selectedPhotoIds.includes(item.id));
+        setItems(filteredArray);
+        console.log('filteredArray', filteredArray);
       }
-    console.log(data)
-    useEffect(() => {
-        return setPhotoData(photoData);
-    }, [])
+
     return (
         <div className="boxContainer">
             <Container>
                 <div className="p-3">
                     <div className="box p-3">
-                        <div className="d-flex justify-content-between algin-items-center ">
-                            <Form.Group className="mb-3" id="formGridCheckbox">
-                                <Form.Check type="checkbox" label="3 selected files" className="fw-bold text-dark" />
+                        <div className="d-flex justify-content-between algin-items-center">
+                            <Form.Group className="mb-3 d-flex justify-content-center align-items-center" id="formGridCheckbox">
+                                <Form.Check type="checkbox" className="fw-bold text-dark"
+                                    label={`${selectedItems.length} selected files`} checked={selectedItems.length > 0 ? true : false} />
                             </Form.Group>
-                            <h6 className="text-danger cursor-pointer">Delete Files</h6>
+                            <Button className="btn_delete" onClick={handleDelete}>Delete Files</Button>
                         </div>
                         <SortableList onSortEnd={onSortEnd} className="list" draggedItemClassName="dragged">
-                        <Row className="gy-2">
-                            <Col md={5} className="">
-                                <Row className="gy-2">
-                                    <Col md={12}>
-                                        <div className="d-flex justify-content-end align-items-center">
-                                            <Image src={items[0]?.photo} alt="image" className="rghtImg" />
-                                        </div>
-                                    </Col>
-                                    {
-                                        items.slice(0, 2).map((item, index: number) => (
-                                            <Col md={6} key={index}>
-                                                <Row className="gy-2 my-2">
-                                                    <Col md={10} className="border">
-                                                        <div className="d-flex justify-content-end align-items-end">
-                                                            <Image src={item?.photo} alt="image" className="rghtBottomImg" />
-                                                        </div>
+                            <Row className="gy-2">
+                                <Col md={5} className="">
+                                    <Row className="gy-2">
+                                        <Col md={12}>
+                                            <div className="d-flex justify-content-end align-items-center">
+                                                <Image src={items[0]?.photo} alt="image" className="rghtImg" />
+                                            </div>
+                                        </Col>
+                                        {
+                                            items.slice(0, 2).map((item, index: number) => (
+                                                <SortableItem key={index}>
+                                                    <Col md={6} >
+                                                        <Row className="gy-2 my-2">
+                                                            <Col md={10} className="border">
+                                                                <div className="d-flex justify-content-end align-items-end">
+                                                                    <Image src={item?.photo} alt="image" className="rghtBottomImg" />
+                                                                </div>
+                                                            </Col>
+                                                        </Row>
                                                     </Col>
-                                                </Row>
-                                            </Col>
-                                        ))
-                                    }
-                                </Row>
-                            </Col>
-                            <Col md={7}>
-                                <Row className="gy-3">
-                                    {
-                                        items.slice(3, 11).map((item, index: number) => (
-                                            <Col md={4} key={index} className="">
-                                                <div className="d-flex justify-content-center align-items-center colRight">
-                                                    <Image src={item?.photo} alt="image" className="leftImg" />
-                                                    <div className="imgOverlay">
-                                                        <Form.Group className="checkbox" id="formGridCheckbox1">
-                                                            <Form.Check type="checkbox" className="m-3"/>
-                                                        </Form.Group>
-                                                    </div>
-                                                </div>
-                                            </Col>
 
-                                        ))
-                                    }
-                                    {/* <div className="imgAdd">
+                                                </SortableItem>
+                                            ))
+                                        }
+                                    </Row>
+                                </Col>
+                                <Col md={7}>
+                                    <Row className="gy-3">
+                                        {
+                                            items.slice(3, 11).map((item, index: number) => (
+                                                <SortableItem key={index}>
+                                                    <Col md={4}>
+                                                        <div className="d-flex justify-content-center align-items-center colRight">
+                                                            <Image src={item?.photo} alt="image" className="leftImg" />
+                                                            <div className="imgOverlay" id={selectedItems.length > 0 ? "checkedValue" : ''}>
+                                                                <Form.Group className="checkbox" id="formGridCheckbox1">
+                                                                    <Form.Check type="checkbox" className='m-3'
+                                                                        onChange={() => handleCheckboxChange(item)}
+                                                                    />
+                                                                </Form.Group>
+                                                            </div>
+                                                        </div>
+                                                  
+                                                    </Col>
+                                                </SortableItem>
+
+                                            ))
+                                        }
+                                        {/* <div className="imgAdd">
                                         <Form.Control type="file" />
                                     </div> */}
-                                </Row>
-                            </Col>
+                                    </Row>
+                                </Col>
 
-                        </Row>
+                            </Row>
 
                         </SortableList>
                     </div>
